6ef5254e73dae8ab3f741db175e98c6406f63ae4
Josh Cook <jcook2303@gmail.com>
2023-02-20 08:45:53 -0800

    Added functionality to display winner on page and disable buttons when a player reaches 5 points.

--- rps.js
+++ rps.js
@@ -1,8 +1,6 @@
 
 const threeChoices = ["Rock", "Paper", "Scissors"];
 
-
-
 function getComputerChoice(threeChoices) {
     return threeChoices[Math.floor(Math.random() * threeChoices.length)];
 }
@@ -37,44 +35,30 @@ function playOneRound(userChoice) {
         return 1;
     }
 };
-/*
-function playGame() {
-    // add eventListeners to the buttons
-     
-
-    playOneRound(userChoice);
-
-
-    
-    
-    
-    buttons.currentTime = 0;
-    
-    return [userCount, computerCount];
-}
-*/
 
 let tieCount = 0;
 let userCount = 0;
 let computerCount = 0;
 let score_count = [];
 const buttons = document.querySelector(".buttons");
+const body = document.querySelector("body");
 
-
-if (userCount === 5 || computerCount === 5) {
-    const newDiv = document.createElement("div");
-    newDiv.classList.add("winner-message");
-    buttons.disabled = true;
-    if (userCount === 5) {
-        newDiv.textContent = "You win!";
+buttons.addEventListener('click', function(e) {
+    const choice = e.target.className;
+    const score = document.querySelector(".score");
+    if (userCount === 5 || computerCount === 5) {
+        const newDiv = document.createElement("div");
+        newDiv.classList.add("winner-message");
+        if (userCount === 5) {
+            newDiv.textContent = "You win!";
+        } else {
+            newDiv.textContent = "Computer wins!";
+        }
+        body.appendChild(newDiv);
+        document.querySelectorAll('.buttons button').forEach(elem => {
+            elem.disabled = true;
+        });
     } else {
-        newDiv.textContent = "Computer wins!";
-    }
-} else {
-    buttons.addEventListener('click', function(e) {
-        const choice = e.target.className;
-        const score = document.querySelector(".score");
-    
         if (choice === "button-rock") {
             userChoice = "rock";
         } else if (choice === "button-paper") {
@@ -94,11 +78,6 @@ if (userCount === 5 || computerCount === 5) {
             computerCount++;
             score.textContent = `${userCount} - ${computerCount}`;
         }
-    userCount = userCount;
-    computerCount = computerCount;
-    console.log(userCount);
-    console.log(computerCount);
-    })
-}
+    }
 
-
+})
